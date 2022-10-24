c4a2f00ff3f9e70d343f4aa403e4c958dc684d9b
saladbowl77 <sarakun.info@gmail.com>
2022-10-25 01:23:53 +0900

    fix : typo

--- src/routes/server2/+page.server.js
+++ src/routes/server2/+page.server.js
@@ -2,7 +2,7 @@ import { createClient } from 'newt-client-js';
 import { env } from '$env/dynamic/private'
 
 export function load() {
-    const num = 1+2+3+4:
+    const num = 1+2+3+4;
 
     return num;
 }
