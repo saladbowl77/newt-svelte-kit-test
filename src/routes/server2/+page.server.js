8ab46852914bf855e705da5270d834591e8821b8
saladbowl77 <sarakun.info@gmail.com>
2022-10-25 01:25:49 +0900

    fix : test

--- src/routes/server2/+page.server.js
+++ src/routes/server2/+page.server.js
@@ -1,8 +1,3 @@
-import { createClient } from 'newt-client-js';
-import { env } from '$env/dynamic/private'
-
 export function load() {
-    const num = 1+2+3+4;
-
-    return num;
+    return 1+2+3+4;
 }
