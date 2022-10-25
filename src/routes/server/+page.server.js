5a6667adc086225e11c8e7a3dc857a353947975f
saladbowl77 <sarakun.info@gmail.com>
2022-10-25 21:39:26 +0900

    update

--- src/routes/server/+page.server.js
+++ src/routes/server/+page.server.js
@@ -1,17 +1,20 @@
 import { createClient } from 'newt-client-js';
 import { env } from '$env/dynamic/private'
+import fetchAdapter from "@vespaiach/axios-fetch-adapter";
 
 export function load() {
     async function getdata() {
         const client = createClient({
             spaceUid: 'test-blog',
             token: env.TOKEN,
-            apiType: 'cdn'
+            apiType: 'cdn',
+            adapter: fetchAdapter,
         });
     
         const res = await client.getContents({
             appUid: 'blog-content',
             modelUid: 'post',
+            adapter: fetchAdapter,
             query: {
                 order: "-uploadDate"
             }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
+	ssr: {
+		noExternal: "@vespaiach/axios-fetch-adapter",
+	},
 	plugins: [sveltekit()]
 };
 
